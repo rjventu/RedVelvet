38344966d11092d5c4e17edd340e32db84dd0a62
rubyyventuraa <venturarubyjean@gmail.com>
2024-01-24 15:44:13 +0800

    Update menu-cheese.php

--- menu-cheese.php
+++ menu-cheese.php
@@ -1,63 +1,16 @@
 <!doctype html>
 <html lang="en">
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles.css">
-    <link rel="icon" type="image/x-icon" href="assets/favicon.jpg">
+<!-- Head -->
+<?php include("head-tags.php")?>
     <title>Red Velvet KH - Cheesecakes</title>
 </head>
   
 <body>
 
-<header>
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top">
-        <a class="navbar-brand" href="index.html">
-            <img src="assets/logo.png" width="250" height="50" alt="">
-          </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item ml-auto">
-              <a class="nav-link" href="index.html">HOME</a>
-            </li>
-            <li class="nav-item ml-auto">
-              <a class="nav-link" href="about.html">ABOUT US</a>
-            </li>
-            <li class="nav-item dropdown ml-auto ">
-              <a class="nav-link dropdown-toggle" id="active" href="menu.html" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                MENU
-              </a>
-              <div class="dropdown-menu text-center" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="menu-sigcakes.html">SIGNATURE CAKES</a>
-                <a class="dropdown-item" href="menu-cakedel.html">CAKE DELIGHTS</a>
-                <a class="dropdown-item" id="active" href="menu-cheese.html">CHEESECAKES</a>
-                <a class="dropdown-item" href="menu-pastries.html">PASTRIES</a>
-                <a class="dropdown-item" href="menu-cupcakes.html">CUPCAKES</a>
-                <a class="dropdown-item" href="menu-cookies.html">COOKIES</a>
-                <a class="dropdown-item" href="menu-bars.html">BARS</a>
-              </div>
-            </li>
-            <li class="nav-item ml-auto">
-                <a class="nav-link" href="contact.html">CONTACT</a>
-            </li>
-            <li class="nav-item ml-auto">
-                <a class="btn" href="order.html" role="button">ORDER</a>
-            </li>
-          </ul>
-        </div>
-    </nav>
-</header>
+<!-- Header -->
+<?php include("header.php")?>
+
 <section class="menu-banner">
     <div>
         <h1>Cheese</h1><span>cakes</span>
