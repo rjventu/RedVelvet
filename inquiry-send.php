9e689bdcd17312482f5d160161bc3dccc9c590b4
rubyyventuraa <venturarubyjean@gmail.com>
2024-02-01 13:46:46 +0800

    configured inquiry form
    + inquiries now sent to company email
    via phpmailer

---
+++ inquiry-send.php
@@ -0,0 +1,75 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+
+function emptyInput(){
+  if(isset($_POST['email']) && isset($_POST['name']) && isset($_POST['subject']) && isset($_POST['message'])){
+    return false;
+  }else{
+    return true;
+  }
+}
+
+function invalidEmail(){
+  if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+
+
+if(isset($_POST['send'])){
+
+  if(emptyInput()){
+    echo  "<script>alert('Error: Please complete all fields.');</script>";
+    exit();
+  }
+  if(invalidEmail()){
+    echo  "<script>alert('Error: Please input a valid email address.');</script>";
+    exit();
+  }
+
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
+
+  $messageNew = "Sender name: " . $name . "<br/>Sender email address: " . $email . "<br/><br/>" . $message;
+
+  $mail = new PHPMailer(true);
+
+  $mail->isSMTP();
+  $mail->SMTPAuth = true;
+
+  $mail->Host = 'smtp.gmail.com';
+  $mail->SMTPSecure = 'ssl';
+  $mail->Port = 465;
+
+  $mail->Username = 'redvelvetkh.inq@gmail.com';
+  $mail->Password = 'jeknjpdzxngpfzdd';
+
+  $mail->setFrom($email);
+  $mail->addAddress('redvelvetkh.inq@gmail.com');
+
+  $mail->isHTML(true);
+  $mail->Subject = $subject;
+  $mail->Body = $messageNew;
+
+  $mail->send();
+
+  echo
+  "
+  <script>
+  alert('Message sent!');
+  document.location.href = 'index.php';
+  </script>
+  ";
+
+}
--- inquiry.php
+++ inquiry.php
@@ -8,20 +8,25 @@
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="inquiry-modal">
-              <h2> Have a question?</h2>
-              <form class="inq-form">
+              <h2 class="mb-4"> Have a question?</h2>
+              <form class="inq-form" method="post" action="inquiry-send.php">
+                <br>
                 <div>
                   <label for="name">Name:</label>
-                  <input type="text" id="name" name="name" placeholder="Enter your name">
+                  <input type="text" id="name" name="name" placeholder="Enter your name" required>
                 </div>
                 <br>
                 <div>
                   <label for="email">Email:</label>
-                  <input type="email" id="email" name="email" placeholder="Enter your email">
+                  <input type="email" id="email" name="email" placeholder="Enter your email" required>
                 </div>
                 <br>
-                <textarea id="message" name="message" rows="4" cols="50" placeholder="Enter your message"></textarea>
-                <input type="submit" value="Submit" class="btn">
+                <div class="mb-4">
+                  <label for="subject">Subject:</label>
+                  <input type="text" id="subject" name="subject" placeholder="Enter message subject" required>
+                </div>
+                <textarea id="message" name="message" rows="5" cols="50" maxlength="1000" placeholder="Enter your message" required></textarea>
+                <button type="submit" name="send" class="btn">Submit</button>
               </form>
             </div>
         </div>
