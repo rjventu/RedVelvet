7c96549c75aa1ef22c39370bd1d7ac6f68ea3aa7
rubyyventuraa <venturarubyjean@gmail.com>
2024-02-01 13:53:11 +0800

    Update inquiry-send.php

--- inquiry-send.php
+++ inquiry-send.php
@@ -30,46 +30,46 @@ if(isset($_POST['send'])){
   if(emptyInput()){
     echo  "<script>alert('Error: Please complete all fields.');</script>";
     exit();
-  }
-  if(invalidEmail()){
+  }else if(invalidEmail()){
     echo  "<script>alert('Error: Please input a valid email address.');</script>";
     exit();
+  }else{
+    
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    
+    $messageNew = "Sender name: " . $name . "<br/>Sender email address: " . $email . "<br/><br/>" . $message;
+    
+    $mail = new PHPMailer(true);
+    
+    $mail->isSMTP();
+    $mail->SMTPAuth = true;
+    
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port = 465;
+    
+    $mail->Username = 'redvelvetkh.inq@gmail.com';
+    $mail->Password = 'jeknjpdzxngpfzdd';
+    
+    $mail->setFrom($email);
+    $mail->addAddress('redvelvetkh.inq@gmail.com');
+    
+    $mail->isHTML(true);
+    $mail->Subject = $subject;
+    $mail->Body = $messageNew;
+    
+    $mail->send();
+    
+    echo
+    "
+    <script>
+    alert('Message sent!');
+    document.location.href = 'index.php';
+    </script>
+    ";
   }
-
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $subject = $_POST['subject'];
-  $message = $_POST['message'];
-
-  $messageNew = "Sender name: " . $name . "<br/>Sender email address: " . $email . "<br/><br/>" . $message;
-
-  $mail = new PHPMailer(true);
-
-  $mail->isSMTP();
-  $mail->SMTPAuth = true;
-
-  $mail->Host = 'smtp.gmail.com';
-  $mail->SMTPSecure = 'ssl';
-  $mail->Port = 465;
-
-  $mail->Username = 'redvelvetkh.inq@gmail.com';
-  $mail->Password = 'jeknjpdzxngpfzdd';
-
-  $mail->setFrom($email);
-  $mail->addAddress('redvelvetkh.inq@gmail.com');
-
-  $mail->isHTML(true);
-  $mail->Subject = $subject;
-  $mail->Body = $messageNew;
-
-  $mail->send();
-
-  echo
-  "
-  <script>
-  alert('Message sent!');
-  document.location.href = 'index.php';
-  </script>
-  ";
-
+  
 }
